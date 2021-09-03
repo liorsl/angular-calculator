1086237fc8c2f040aa031c2a31b0edaa1b119cae
Lior S <liorsl@protonmail.com>
2021-09-03 13:52:36 +0300

    Initial prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css']
 })
 export class AppComponent {
-  title = 'calc';
+  displayValue = '';
+
+  handleNumberPress(num: number) {
+    this.append(num);
+
+  }
+
+  handlePress(exp: string) {
+    this.append(exp);
+
+  }
+
+  handleDelete() {
+    if (!this.displayValue.length)
+      return;
+
+    this.displayValue = this.displayValue.substr(0, this.displayValue.length - 1);
+
+  }
+
+  handleClear() {
+    this.displayValue = '';
+  }
+
+  evaluateExpression() {
+    this.displayValue = String(evaluate(this.displayValue));
+  }
+
+  private append(exp: any) {
+    this.displayValue += exp;
+
+  }
+
 }
